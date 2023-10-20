f8f0f6d2d383ea09ed371ffe13ef53ced3ff5793
ayush <ayush@gmail.com>
2023-10-20 23:08:46 +0530

    Updated file name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/React-NotesApp/",
+  base:"/react-notesapp/",
   plugins: [react()],
 })
